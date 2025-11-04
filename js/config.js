0dd86766b718827b2de453394b7485ba9df9f803
bibek.gurung <bibek.gurung@3ptrades.com>
2025-11-05 01:22:19 +0545

    add cognito config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "ap-south-1_j50BPu0uZ", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "1dhauc13e90li5s1135ka2mmhp", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-south-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
